8752176d6be7cbd64d51a117d7ad6ebeccda1160
Mat191126 <matprz11@gmail.com>
2021-08-20 12:22:40 +0200

    Resolve problem with displaying images, add logo image

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import MainMenu from "./MainMenu";
+import logo from "../assets/images/logo.png";
 
 function Header() {
     return (
         <>
             <header className="header">
-                <div id="main_content">
+                <div id="header_content">
                     <div id="header_and_navbar">
                         <div id="header">
-                            <p>HEADER IMAGE</p>
+                            <img id="logo" src={logo} alt="logo image"/>
+                            <p>STAJNIA POD <br/> WIŚLANYM WAŁEM</p>
                         </div>
                         <MainMenu />
                     </div>
