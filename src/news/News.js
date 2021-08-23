605ace329b90d47dbe6da0959f0b2c74fbf13513
Mat191126 <matprz11@gmail.com>
2021-08-23 13:05:17 +0200

    Add News page content

--- src/news/News.js
+++ src/news/News.js
@@ -4,11 +4,21 @@ function News() {
     return (
         <>
             <div id="content">
-                <p>News page</p>
-                <p>News page</p>
-                <p>News page</p>
-                <p>News page</p>
-                <p>News page</p>
+                <div className="advertisement">
+                    <h2>NOWY CENNIK</h2>
+                    <p className="advertisement_content">
+                        Drodzy Państwo,<br/>
+                        od 4 maja 2020 zmieniamy formę zajęć w naszej stajni. Prowadzimy jazdy w parach oraz całkowicie
+                        indywidualne. Będą to oczywiście zajęcia na każdym poziomie, od oprowadzanek poprzez lonżę do j
+                        azd samodzielnych rekreacyjnych oraz w kierunku ujeżdżenia i skoków. Tak jak do tej pory będziemy
+                        również organizować konsultacje sportowe oraz wyjazdy szkoleniowe. Ze względu na komfort
+                        wszystkich użytkowników naszej stajni rezygnujemy z jazd grupowych.
+                        <br/>
+                        Serdecznie zapraszamy do zapisów i ustalenia stałych terminów. Dotychczasowe terminy grup
+                        są nieaktualne. Tworzymy nowy grafik.<br/>
+                        Do zobaczenia!
+                    </p>
+                </div>
             </div>
         </>
     );
