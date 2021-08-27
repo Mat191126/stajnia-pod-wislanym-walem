93802c4a801aecc1d74b4f4a93c91c973ba08dc5
Mat191126 <matprz11@gmail.com>
2021-08-27 08:25:09 +0200

    Change logo, add video to about section

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,23 +2,34 @@ import React from 'react';
 import rider from "../assets/images/rider.png";
 import earth from "../assets/images/earth.png";
 import star from "../assets/images/star.png";
+import aboutVideo from "../assets/video/about-video-cut.mp4";
 
 function Home() {
+
+    document.addEventListener('DOMContentLoaded', function(event) {
+        document.querySelector('video').playbackRate = 0.5;
+    });
+
     return (
         <>
             <div id="content">
                 <div id="about_section">
                     <h2 id="about_header">Poznaj naszą stajnię!</h2>
-                    <p id="about_description">Nasza stajnia oferuje naukę jazdy konnej dla dzieci, młodzieży oraz dorosłych.
-                        Spokojna i cicha okolica stwarza warunki idealne do aktywnego wypoczynku, a przede wszystkim
-                        zabawy na końskim grzbiecie.
-                        <br/>
-                        Staramy się, aby pobyt u nas był przyjemny i relaksujący, a także pozwolił zdobyć nowe umięjetności.
-                        Miłośnicy spacerów i rowerów mogą zostawić swoje pociechy u nas, a sami udać się na
-                        wspaniałą ścieżkę na wale przeciwpowodziowym, która znajduje się 150m od stajni.
-                        <br/>
-                        Zapraszamy wszystkich, którzy marzą o spędzaniu czasu wśród koni i poznaniu ich wspaniałego świata.
-                        Żadna minuta Pod Wiślanym Wałem nie będzie nudna!</p>
+                    <div id="about_content">
+                        <div id="about-video" >
+                            <video autoPlay loop muted playsInline src={aboutVideo}/>
+                        </div>
+                        <p id="about_description">Nasza stajnia oferuje naukę jazdy konnej dla dzieci, młodzieży oraz dorosłych.
+                            Spokojna i cicha okolica stwarza warunki idealne do aktywnego wypoczynku, a przede wszystkim
+                            zabawy na końskim grzbiecie.
+                            <br/>
+                            Staramy się, aby pobyt u nas był przyjemny i relaksujący, a także pozwolił zdobyć nowe umięjetności.
+                            Miłośnicy spacerów i rowerów mogą zostawić swoje pociechy u nas, a sami udać się na
+                            wspaniałą ścieżkę na wale przeciwpowodziowym, która znajduje się 150m od stajni.
+                            <br/>
+                            Zapraszamy wszystkich, którzy marzą o spędzaniu czasu wśród koni i poznaniu ich wspaniałego świata.
+                            Żadna minuta Pod Wiślanym Wałem nie będzie nudna!</p>
+                    </div>
                 </div>
                 <div id="goals_section">
                     <div className="goal">
