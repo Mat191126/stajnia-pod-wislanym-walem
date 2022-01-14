e783cf65cdd237ceb6243758b8f6068496da33fe
Mat191126 <matprz11@gmail.com>
2022-01-14 17:21:49 +0100

    Refactor of gallery page (add ready gallery component)

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -1,68 +1,39 @@
-import React, { useEffect } from "react";
+import React from "react";
 import galleryImagesSources from "./galleryImagesSources";
-
-//First element index
-const FIRST_ELEMENT = 0;
+import ImageGallery from "react-image-gallery";
+import "./imageGalleryStyles/image-gallery.scss";
 
 function Gallery() {
 
-    useEffect(() => {
-        renderGallery();
-    });
-    document.addEventListener('DOMContentLoaded', function(event) {
-        window.onload = function() {
-            renderGallery();
-        }
-    });
-
-    const renderGallery = function() {
-        //Get the main gallery container
-        let galleryContainer = document.getElementById("gallery-content");
-
-        if(document.readyState === 'complete') {
-            for (let albumIndex = 0; albumIndex < galleryImagesSources.length; albumIndex++) {
-                const albumDiv = document.createElement("div");
-                    albumDiv.classList.add("album-content");
-                const albumTitle = document.createElement("h3");
-                    albumTitle.classList.add("album-title");
-                    albumTitle.innerText = galleryImagesSources[albumIndex].album_name;
-                const albumCover = document.createElement("img");
-                    albumCover.classList.add("album-cover");
-                    albumCover.alt = "Album cover image";
-                    albumCover.src = galleryImagesSources[albumIndex].images[FIRST_ELEMENT].image_url;
-                    albumCover.onclick = showImages;
-                const imageContainer = document.createElement("div");
-                    imageContainer.classList.add("image-container");
-                    imageContainer.classList.add("image-container-invisible");
-
-                albumDiv.appendChild(albumTitle);
-                albumDiv.appendChild(albumCover);
-                albumDiv.appendChild(imageContainer);
-                for (let imageIndex = 0; imageIndex < galleryImagesSources[albumIndex].images.length; imageIndex++) {
-                    const singleImage = document.createElement("img");
-                        singleImage.classList.add("single-image");
-                        singleImage.alt = "Album image";
-                        singleImage.src = galleryImagesSources[albumIndex].images[imageIndex].image_url;
-
-                    imageContainer.appendChild(singleImage);
-                }
-                galleryContainer.appendChild(albumDiv);
-            }
-        }
-    }
-
-    function showImages(e) {
-        e.preventDefault();
-        const imageContainer = e.target.parentElement.getElementsByClassName("image-container")[FIRST_ELEMENT];
-        if (imageContainer.classList.contains("image-container-invisible")) {
-            imageContainer.classList.remove("image-container-invisible");
-        } else {
-            imageContainer.classList.add("image-container-invisible");
-        }
-    };
-
     return (
-        <div id="gallery-content"/>
+        <>
+        <div id="gallery-content">
+            <div id="album-1" className="album">
+                <h2 className="album-title">{galleryImagesSources[0].album_name}</h2>
+                <div className="gallery-slider">
+                    <ImageGallery items={galleryImagesSources[0].images} />
+                </div>
+            </div>
+            <div id="album-2" className="album">
+                <h2 className="album-title">{galleryImagesSources[1].album_name}</h2>
+                <div className="gallery-slider">
+                    <ImageGallery items={galleryImagesSources[1].images} />
+                </div>
+            </div>
+            <div id="album-3" className="album">
+                <h2 className="album-title">{galleryImagesSources[2].album_name}</h2>
+                <div className="gallery-slider">
+                    <ImageGallery items={galleryImagesSources[2].images} />
+                </div>
+            </div>
+            <div id="album-4" className="album">
+                <h2 className="album-title">{galleryImagesSources[3].album_name}</h2>
+                <div className="gallery-slider">
+                    <ImageGallery items={galleryImagesSources[3].images} />
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
 
--- src/gallery/galleryImagesSources.js
+++ src/gallery/galleryImagesSources.js
@@ -3,13 +3,16 @@ const galleryImageSources = [
         album_name: "Stajnia",
         images: [
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/49735090_717053278667026_8098125626466107392_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=70wXshqoXEkAX82pHMl&_nc_ht=scontent-vie1-1.xx&oh=00_AT9vo6buf7RRxYWoZaT2UAlAELWVlsgFbFcZlpbCTWCeVA&oe=62006333"
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/49735090_717053278667026_8098125626466107392_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=70wXshqoXEkAX82pHMl&_nc_ht=scontent-vie1-1.xx&oh=00_AT9vo6buf7RRxYWoZaT2UAlAELWVlsgFbFcZlpbCTWCeVA&oe=62006333"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/49735090_717053278667026_8098125626466107392_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=70wXshqoXEkAX82pHMl&_nc_ht=scontent-vie1-1.xx&oh=00_AT9vo6buf7RRxYWoZaT2UAlAELWVlsgFbFcZlpbCTWCeVA&oe=62006333"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             }
         ]
     },
@@ -17,22 +20,24 @@ const galleryImageSources = [
         album_name: "Jazda",
         images: [
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
-            },
-            {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/73147490_902945456744473_717762401792950272_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=CqY_sWXXPHAAX9pK0cz&_nc_ht=scontent-vie1-1.xx&oh=00_AT9DHZUsViZXEfs49JKCRFCQ0olxgDzIbUvAhAmEEsEbNA&oe=62029857"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             }
         ]
     },
@@ -40,34 +45,36 @@ const galleryImageSources = [
         album_name: "Okolica",
         images: [
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
-            },
-            {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
-            },
-            {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             }
         ]
     },
@@ -75,16 +82,20 @@ const galleryImageSources = [
         album_name: "Wydarzenia",
         images: [
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
             },
             {
-                image_url: "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/52598100_747151358990551_1519579826124488704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=sYtjOYAkT28AX9uyIpv&_nc_ht=scontent-vie1-1.xx&oh=00_AT-hhvg5oG0l3lmpxh9PcPo3vTiqzKgnrCUJ_GfyES8RHg&oe=6201B1AB"
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
             }
         ]
     }
