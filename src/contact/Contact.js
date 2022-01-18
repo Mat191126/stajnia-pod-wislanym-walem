76b032faaee09018219002b5c4ff9178b6b2b26d
Mat191126 <matprz11@gmail.com>
2022-01-18 18:09:37 +0100

    Contact page content refactor

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -12,14 +12,25 @@ function Contact() {
             <p id="scroll-to-element"></p>
             <div id="content">
                 <div id="contact-content">
-                    <h2 id="contact_header">Kontakt</h2>
-                    <address>
-                        <p>Stajnia pod wiślanym wałem</p>
-                        <p>ul.Wiślana 34</p>
-                        <p>32-051 Jaśkowice</p>
-                        <p><a href="mailto:stajnia@podwislanymwalem.pl">stajnia@podwislanymwalem.pl</a> </p>
-                        <a href="tel:+48-665-246-359">665 246 359</a>
-                    </address>
+                <h2 id="contact_header">Kontakt</h2>
+                    <div id="contact-part">
+                        <div id="address-content">
+                            <address>
+                            <h3 id="company-header">Stajnia pod wiślanym wałem</h3>
+                            <p className="address-line">ul.Wiślana 34</p>
+                            <p className="address-line">32-051 Jaśkowice</p>
+                            </address>
+                        </div>
+                        <div id="email-content">
+                            <p><a href="mailto:stajnia@podwislanymwalem.pl">stajnia@podwislanymwalem.pl</a></p>
+                        </div>
+                        <div id="phone-content">
+                            <p><a href="tel:+48-665-246-359">665 246 359</a></p>
+                        </div>
+                    </div>
+                    <div id="map-part">
+
+                    </div>
                 </div>
             </div>
         </>
