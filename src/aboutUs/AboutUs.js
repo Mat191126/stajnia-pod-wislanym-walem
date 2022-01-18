29f5e8def910abc8b445879072e9aecd80ffd4ff
Mat191126 <matprz11@gmail.com>
2022-01-18 16:24:33 +0100

    Add sub-menu on About us page

--- src/aboutUs/AboutUs.js
+++ src/aboutUs/AboutUs.js
@@ -17,14 +17,18 @@ function AboutUs() {
             <p id="scroll-to-element"></p>
             <div id="content">
                 <div id="section-menu-buttons">
-                    <div className='link-to-section'><a href='#about-us-section-content'> O nas </a></div>
-                    <div className='link-to-section'><a href='#our-instructors-section-content'> Nasi Instruktorzy </a></div>
-                    <div className='link-to-section'><a href='#our-horses-section-content'> Konie </a></div>
-                    <div className='link-to-section'><a href='#other-animals-section-content'> Inne Zwierzęta </a></div>
+                    <div className='link-to-section'><a href='#scroll-to-about-description-element'> O nas </a><div className="rounded-corners"/></div>
+                    <div className='link-to-section'><a href='#scroll-to-instructors-element'> Nasi Instruktorzy </a><div className="rounded-corners"/></div>
+                    <div className='link-to-section'><a href='#scroll-to-horses-element'> Konie </a><div className="rounded-corners"/></div>
+                    <div className='link-to-section'><a href='#scroll-to-animals-element'> Inne Zwierzęta </a><div className="rounded-corners"/></div>
                 </div>
+                <p id="scroll-to-about-description-element"></p>
                 <AboutDescriptionSection />
+                <p id="scroll-to-instructors-element"></p>
                 <OurInstructorsSection />
+                <p id="scroll-to-horses-element"></p>
                 <OurHorsesSection />
+                <p id="scroll-to-animals-element"></p>
                 <OtherAnimalsSection />
             </div>
         </>
