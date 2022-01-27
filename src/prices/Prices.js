600ba04fa5e466238fd4e83de17bf8622cae6d4e
Mat191126 <matprz11@gmail.com>
2022-01-27 16:07:19 +0100

    Add price temporary table

--- src/prices/Prices.js
+++ src/prices/Prices.js
@@ -13,13 +13,55 @@ function Prices() {
             <div id="content">
                 <div id="prices-content">
                     <h2 id="prices-header">Cennik</h2>
-                    <h3>Jazdy Pojedyncze</h3>
-                    <p>Jazda dla początkujących (lonża) 30 min - 65zł</p>
-                    <p>Jazda indywidualna: jeżdziec i instruktor 60 min - 120zł</p>
-                    <p>Jazda indywidualna w parze: 2 jeźdźców i instruktor 60 min - 90zł /osoba</p>
-                    <p>Teren 60 min - 90zł</p>
-                    <p>Oprowadzanie na kucyku 10 min - 35zł</p>
-                    <p>Oprowadzanie + zajęcia z kucykiem dla maluchów ok 30 min - 65zł</p>
+                    <div id="single-rides-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Jazda</th>
+                                    <th>Czas</th>
+                                    <th>Cena</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td rowspan="6" className="vertical-table-header">Jazdy Pojedyncze</td>
+                                    <td>Jazda dla początkujących (lonża)</td>
+                                    <td>30 min</td>
+                                    <td>65zł</td>
+                                </tr>
+                                <tr>
+                                    <td>Jazda indywidualna: jeżdziec i instruktor</td>
+                                    <td>60 min</td>
+                                    <td>120zł</td>
+                                </tr>
+                                <tr>
+                                    <td>Jazda w parze: 2 jeźdźców i instruktor</td>
+                                    <td>60 min</td>
+                                    <td>90zł /osoba</td>
+                                </tr>
+                                <tr>
+                                    <td>Jazda w terenie</td>
+                                    <td>60 min</td>
+                                    <td>90zł</td>
+                                </tr>
+                                <tr>
+                                    <td>Oprowadzanie na kucyku</td>
+                                    <td>10 min</td>
+                                    <td>35zł</td>
+                                </tr>
+                                <tr>
+                                    <td>Oprowadzanie + zajęcia z kucykiem dla maluchów</td>
+                                    <td>ok 30 min</td>
+                                    <td>65zł</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="membership-table">
+
+                    </div>
+                    
                     <h3>Karnety</h3>
                     <p>Karnet 8x 30min (ważny 8 tygodni) - 440zł</p>
                     <p>Karnet 8x 30min (ważny 4 tygodnie) - 400zł</p>
