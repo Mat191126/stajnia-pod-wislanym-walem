d5f3332aee6d03287b6897aad8da25cd2ed616d9
Mat191126 <matprz11@gmail.com>
2022-01-27 15:00:37 +0100

    Instructors styles conflict refactor

--- src/aboutUs/sections/OurInstructorsSection.js
+++ src/aboutUs/sections/OurInstructorsSection.js
@@ -7,35 +7,35 @@ function OurInstructorsSection() {
     return (
         <>
             <div id="our-instructors-section-content">
-                <div className="single-instructor">
-                    <div className="instructor-image-part" >
-                        <img className="instructor-image" src={ExampleImage} alt="" />
+                <div className="our-instructors-single-instructor">
+                    <div className="our-instructors-instructor-image-part" >
+                        <img className="our-instructors-instructor-image" src={ExampleImage} alt="" />
                     </div>
-                    <div className="instructor-description-part">
-                        <h1 className="instructor-data">Katarzyna Legień</h1>
-                        <p className="instructor-description">
+                    <div className="our-instructors-instructor-description-part">
+                        <h1 className="our-instructors-instructor-data">Katarzyna Legień</h1>
+                        <p className="our-instructors-instructor-description">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
                 </div>
-                <div className="single-instructor">
-                    <div className="instructor-image-part" >
-                        <img className="instructor-image" src={ExampleImage} alt="" />
+                <div className="our-instructors-single-instructor">
+                    <div className="our-instructors-instructor-image-part" >
+                        <img className="our-instructors-instructor-image" src={ExampleImage} alt="" />
                     </div>
-                    <div className="instructor-description-part">
-                        <h1 className="instructor-data">Małgorzata Nowak</h1>
-                        <p className="instructor-description">
+                    <div className="our-instructors-instructor-description-part">
+                        <h1 className="our-instructors-instructor-data">Małgorzata Nowak</h1>
+                        <p className="our-instructors-instructor-description">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
                 </div>
-                <div className="single-instructor">
-                    <div className="instructor-image-part" >
-                        <img className="instructor-image" src={ExampleImage} alt="" />
+                <div className="our-instructors-single-instructor">
+                    <div className="our-instructors-instructor-image-part" >
+                        <img className="our-instructors-instructor-image" src={ExampleImage} alt="" />
                     </div>
-                    <div className="instructor-description-part">
-                        <h1 className="instructor-data">Emilia Kowalska</h1>
-                        <p className="instructor-description">
+                    <div className="our-instructors-instructor-description-part">
+                        <h1 className="our-instructors-instructor-data">Emilia Kowalska</h1>
+                        <p className="our-instructors-instructor-description">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
