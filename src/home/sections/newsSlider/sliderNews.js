a5cb1641482483a4a8e14b663b4a2a3dadd092e9
Mat191126 <matprz11@gmail.com>
2022-01-28 15:51:49 +0100

    News data refactor

--- src/home/sections/newsSlider/sliderNews.js
+++ src/home/sections/newsSlider/sliderNews.js
@@ -3,8 +3,8 @@ import ExampleImage2 from "../../../assets/images/sliderImages/image2.jpg";
 
 const sliderNews = [
     {
-        id: 1,
-        title: "Happy New Year!",
+        id: 311220211,
+        title: "Szcęśliwego Nowego Roku!",
         date: "31.12.2021",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         image_url: ExampleImage2,
@@ -12,24 +12,24 @@ const sliderNews = [
     },
     {
         id: 2,
-        title: "Merry Christmas!",
+        title: "Wesołych Świąt!",
         date: "23.12.2021",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         image_url: ExampleImage2,
         addon_image_url: ExampleImage1
     },
     {
-        id: 3,
-        title: "Prizes change",
+        id: 171220211,
+        title: "Zmiana cen",
         date: "17.12.2021",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         image_url: ExampleImage2,
         addon_image_url: ExampleImage1
     },
     {
-        id: 4,
-        title: "New working hours",
-        date: "10.12.2021",
+        id: 171220212,
+        title: "Nowe godziny otwarcia",
+        date: "17.12.2021",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         image_url: ExampleImage2,
         addon_image_url: ExampleImage1
